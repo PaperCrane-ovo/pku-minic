c3959f7de248487ff26302358e3417e62e560844
Crane <15352461680@126.com>
2024-04-23 16:37:55 +0800

    none

--- build.rs
+++ build.rs
@@ -3,13 +3,5 @@ use lettre::{message::header::ContentType, transport::smtp::{authentication::Cre
 fn main() {
     lalrpop::process_root().unwrap();
     
-    // let email = Message::builder()
-    //     .from("crane cranesweetie@gmail.com".parse().unwrap())
-    //     .to("crane 15352461680@126.com".parse().unwrap())
-    //     .subject("Hello")
-    //     .header(ContentType::TEXT_PLAIN)
-    //     .body(String::from("Hello, world!"))
-    //     .unwrap();
-    // let creds = Credentials::new(username,password);
 }
   
