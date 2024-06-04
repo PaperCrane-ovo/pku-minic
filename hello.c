30bd8689b6065c71a4560be746f3c936fddf5ee3
Crane <15352461680@126.com>
2024-06-04 23:04:25 +0800

    many_param?

--- hello.c
+++ hello.c
@@ -1,7 +1,18 @@
-int a(int a1,int a2,int a3,int a4){
-	return a1+a2;
+int sum(int a0, int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8,
+         int a9, int a10, int a11, int a12, int a13, int a14, int a15,int a16) {
+  return a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10 + a11 + a12 +
+         a13 + a14 + a15 + a16;
 }
 
-int b(int b1,int b2){
-	return a(b1,b2);
+int sum2(int a0, int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8,
+         int a9, int a10, int a11, int a12, int a13, int a14, int a15) {
+  return a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10 + a11 + a12 +
+         a13 + a14 + a15;
+}
+
+int main() {
+  int x = sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
+  int y = sum2(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+  putint(x+y);
+  return x + y;
 }
