035ed129780bf227cacd6e8677bfbae7435b3cad
Crane <15352461680@126.com>
2024-06-10 01:49:04 +0800

    fix stackframe overflow

--- src/ir2riscv/stack.rs
+++ src/ir2riscv/stack.rs
@@ -58,11 +58,12 @@ impl StackFrame{
             }
         }
         
-        self.size = size  + {if call {4} else {0}} + {if param_count > 8 {4*(param_count-8)} else {0}};
+        self.size = size+32  + {if call {4} else {0}} + {if param_count > 8 {4*(param_count-8)} else {0}};
         self.size = if self.size % 16 == 0 {self.size} else {self.size/16*16+16};
         self.call = call;
         self.param_pos = 0;
         self.pos = self.size as i32 -4;
+        dbg!(func.name(),self.size);
         self.size
     }
     pub fn new() -> Self{
